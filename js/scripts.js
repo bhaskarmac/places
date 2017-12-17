3d15c0c513c89efc28f7276267a88165e3037acf
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-12-17 21:46:47 +0530

    Adding basic map with some settings

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,22 @@
 console.log('scripts loaded');
 console.log('mapsKey=>', mapsKey);
+
+//loading and adding the Google maps js to DOM
+var mapJSRef = document.createElement('script');
+mapJSRef.setAttribute("type", "text/javascript");
+mapJSRef.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key='+ mapsKey.key +'&libraries=places');
+mapJSRef.onload = function () {
+	console.log('mapJSRef loaded');
+	initMap();
+};
+document.getElementsByTagName("head")[0].appendChild(mapJSRef);
+
+var mapInstance;
+
+function initMap() {
+	//Creating map and setting options
+	mapInstance = new google.maps.Map(document.getElementById('map'), {
+		center: {lat: 18.5204, lng: 73.8567},
+		zoom: 8
+	});
+}
