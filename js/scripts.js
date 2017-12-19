c2d0a190363c39f501907bf8337f2c6287d6bdc9
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-12-19 12:14:52 +0530

    Adding feature of detecting geolocation and setting in map

--- js/scripts.js
+++ js/scripts.js
@@ -7,28 +7,12 @@ mapJSRef.setAttribute("type", "text/javascript");
 mapJSRef.setAttribute('src', 'https://maps.googleapis.com/maps/api/js?key='+ mapsKey.key +'&libraries=places');
 mapJSRef.onload = function () {
 	console.log('mapJSRef loaded');
-	initMap();
+  getGeoLocation();
 };
 document.getElementsByTagName("head")[0].appendChild(mapJSRef);
 
 var mapInstance;
 
-function initMap() {
-	//Creating map and setting options
-	mapInstance = new google.maps.Map(document.getElementById('map'), {
-		center: {lat: 18.5204, lng: 73.8567},
-		zoom: 8
-	});
-
-	var addressURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=18.5204,73.8567&radius=500&type=restaurant&key='+ mapsKey.key;
-
-	get(addressURL).then(function(response) {
-		var finalResults = JSON.parse(response);
-		console.log('finalResults=>', finalResults);
-	}, function(error) {
-		console.log("Promise failed=>", error);
-	});
-}
 
 /**
  * [get method for XHR]
@@ -65,3 +49,50 @@ function initMap() {
     req.send();
   });
 } //get ends here
+
+
+function getGeoLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(setLocationByGeo, showError);
+  } else {
+    console.log('Something went wrong!');
+  }
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+    console.log("User denied the request for Geolocation.");
+    break;
+    case error.POSITION_UNAVAILABLE:
+    console.log("Location information is unavailable.");
+    break;
+    case error.TIMEOUT:
+    console.log("The request to get user location timed out.");
+    break;
+    case error.UNKNOWN_ERROR:
+    console.log("An unknown error occurred.");
+    break;
+  }
+}
+
+function setLocationByGeo(position) {
+  console.log('currentPosition=>', position);
+  
+  var currentPosition = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude
+  };
+
+  mapInstance = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: currentPosition.latitude, lng: currentPosition.longitude},
+    zoom: 15
+  });
+
+  otherThings();
+}
+
+function otherThings() {
+ var addressURL = 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=18.5204,73.8567&radius=500&type=restaurant&key='+ mapsKey.key;
+
+}
