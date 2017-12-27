7e934c6a3a31907dcad692c364a1b3609977d8f4
Bhaskar Machcha <bhaskarmachcha2@gmail.com>
2017-12-27 17:24:13 +0530

    Updating build

--- js/scripts.js
+++ js/scripts.js
@@ -14,6 +14,7 @@ mapJSRef.onload = function () {
 document.getElementsByTagName("head")[0].appendChild(mapJSRef);
 
 var mapInstance, infowindow, currentLocationObj, markers = [];
+var radiusSlider, txtsearchRadius;
 
 function loadPlaceTypes() {
   if(placeTypes.length > 0){
@@ -33,42 +34,6 @@ function getSelectedPlace() {
   searchPlaces(selectedValue);
 }
 
-/**
- * [get method for XHR]
- * @param  {[string]} url [accepts the url to make call]
- * @return {[Promise]}     [returns Promise of call]
- */
- function get(url) {
-  // Return a new promise.
-  return new Promise(function(resolve, reject) {
-    // Do the usual XHR stuff
-    var req = new XMLHttpRequest();
-    req.open('GET', url);
-
-    req.onload = function() {
-      // This is called even on 404 etc
-      // so check the status
-      if (req.status == 200) {
-        // Resolve the promise with the response text
-        resolve(req.response);
-      }
-      else {
-        // Otherwise reject with the status text
-        // which will hopefully be a meaningful error
-        reject(Error(req.statusText));
-      }
-    };
-
-    // Handle network errors
-    req.onerror = function() {
-    	reject(Error("Network Error"));
-    };
-
-    // Make the request
-    req.send();
-  });
-} //get ends here
-
 function getGeoLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(setLocationByGeo, showError);
@@ -78,18 +43,23 @@ function getGeoLocation() {
 }
 
 function showError(error) {
+  var errorDiv = document.getElementById("errorDiv");
   switch(error.code) {
     case error.PERMISSION_DENIED:
     console.log("User denied the request for Geolocation.");
+    errorDiv.innerHTML = "User denied the request for Geolocation.";
     break;
     case error.POSITION_UNAVAILABLE:
     console.log("Location information is unavailable.");
+    errorDiv.innerHTML = "Location information is unavailable.";
     break;
     case error.TIMEOUT:
     console.log("The request to get user location timed out.");
+    errorDiv.innerHTML = "The request to get user location timed out.";
     break;
     case error.UNKNOWN_ERROR:
     console.log("An unknown error occurred.");
+    errorDiv.innerHTML = "An unknown error occurred.";
     break;
   }
 }
@@ -112,12 +82,41 @@ function setLocationByGeo(position) {
 
 function searchPlaces(strPlaceType) {
   console.log('in searchPlaces=>', strPlaceType);
+  var searchRadiusElValue = document.getElementById('searchRadius').value;
+  var radiusSlider = document.querySelector('#radiusSlider'),
+  txtsearchRadius = document.querySelector('#searchRadius');
+  
+  if(!searchRadiusElValue){
+    searchRadiusElValue = 2;
+    document.getElementById('searchRadius').value = 2;
+
+    radiusSlider.value = 2;
+    txtsearchRadius.innerHTML = radiusSlider.value;
+  }
 
   infowindow = new google.maps.InfoWindow();
   var service = new google.maps.places.PlacesService(mapInstance);
+
+  radiusSlider.addEventListener('change', function () {
+    console.log('radiusSlider.value=>', radiusSlider.value);
+    txtsearchRadius.value = radiusSlider.value;
+    searchRadiusElValue = radiusSlider.value;
+
+    var radiusInMeter = searchRadiusElValue * 1000;
+
+    service.nearbySearch({
+      location: currentLocationObj,
+      radius: radiusInMeter,
+      type: [strPlaceType]
+    }, cbResults);
+
+  }, false);
+
+  var radiusInMeter = searchRadiusElValue * 1000;
+
   service.nearbySearch({
     location: currentLocationObj,
-    radius: 2000,
+    radius: radiusInMeter,
     type: [strPlaceType]
   }, cbResults);
 
@@ -126,11 +125,11 @@ function searchPlaces(strPlaceType) {
 function cbResults(results, status) {
   console.log('in cbResults=>');
   if(results.length > 0){
+    document.getElementById('placeCount').innerHTML = '(' + results.length + ')';
     clearMarkers();
     if (status === google.maps.places.PlacesServiceStatus.OK) {
       for (var i = 0; i < results.length; i++) {
         console.log('results[i].name=>', results[i].name);
-        // console.log('results[i]=>', results[i]);
         addMarker(results[i]);
       }
     }
